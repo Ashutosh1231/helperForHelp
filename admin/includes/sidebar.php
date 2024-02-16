e9749356843878c1e72cedbf6070814bace297ea
Nikhil <nikhil.rajhans@gmail.com>
2024-02-16 12:46:04 +0530

    sidebar change

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,5 +1,5 @@
 <div id="logo-sidebar" class="fixed md:relative top-0 left-0 z-40 w-64 h-screen md:pt-5 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
-
+    <!---->
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
             <li>
